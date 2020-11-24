0aa430d1331ed4f9ca3d543d511ffef6550dd28b
Aravind-2001 <74911640+Aravind-2001@users.noreply.github.com>
2020-11-24 18:09:32 +0530

    Add files via upload

---
+++ 1)basic program.py
@@ -0,0 +1,15 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=10
+>>> b=20
+>>> c=30
+>>> a=a/10
+>>> print(a)
+1.0
+>>> b=b*50
+>>> print(b)
+1000
+>>> c=c+50
+>>> print(c)
+80
+>>> 
---
+++ 3) int float.py
@@ -0,0 +1,9 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=10
+>>> b=20.20
+>>> print(float(a))
+10.0
+>>> print(int(b))
+20
+>>> 
