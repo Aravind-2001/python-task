7985fcfd2c181bd1ed2d6ce1cf167a3b7c59e417
Aravind-2001 <74911640+Aravind-2001@users.noreply.github.com>
2020-11-27 20:34:21 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,8 +1,7 @@
-#Python program to sub two numbers using function
-
-def sub_num(a,b):
-    sum=a-b;
-    return sum; 
-num1=int(input("input the number one: "))
-num2=int(input("input the number one :"))
-print("The sum is",sub_num(num1,num2))
+def covid(patient_name,temp="98deg"):
+     print("Patient Name is",patient_name,"and temperature is",temp)
+     covid("ravi")
+     covid("som")
+     covid("kavi")
+     covid("meenu")
+     covid("aravind","100deg")
